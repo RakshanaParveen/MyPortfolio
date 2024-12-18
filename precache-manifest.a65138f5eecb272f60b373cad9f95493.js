75ed0a9311b8cf84e0e9823d3a89cefb659b1597
RakshanaParveen <rakshanaparveen14@gmail.com>
2024-12-18 15:34:19 +0530

    Updates

---
+++ precache-manifest.a65138f5eecb272f60b373cad9f95493.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863ddd491fc5f24b5b3004c55c0e0cd6",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "18969fc135a8ed17c37a",
+    "url": "/MyPortfolio/static/css/main.7ed3407b.chunk.css"
+  },
+  {
+    "revision": "52770908cca6ce78f266",
+    "url": "/MyPortfolio/static/js/2.646cdce0.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/MyPortfolio/static/js/2.646cdce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18969fc135a8ed17c37a",
+    "url": "/MyPortfolio/static/js/main.459ad54e.chunk.js"
+  },
+  {
+    "revision": "183535680df6aa7b7ab9",
+    "url": "/MyPortfolio/static/js/runtime-main.0ecf43d4.js"
+  },
+  {
+    "revision": "6962dadffd8ea72df5952d4fd7b02f1d",
+    "url": "/MyPortfolio/static/media/Rakshana Parveen Resume.6962dadf.pdf"
+  },
+  {
+    "revision": "f1b8362b64892b5a74794bba04ca6fd7",
+    "url": "/MyPortfolio/static/media/kk.f1b8362b.jpg"
+  },
+  {
+    "revision": "faf3c5588fd3b584dc6aeac1f89cec7f",
+    "url": "/MyPortfolio/static/media/nknk.faf3c558.jpg"
+  },
+  {
+    "revision": "e5391ce398e43740f68982a319f86457",
+    "url": "/MyPortfolio/static/media/portfolio11.e5391ce3.jpg"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/MyPortfolio/static/media/portfolio2.32a90284.jpg"
+  },
+  {
+    "revision": "a0d1b76b4e7fa00495a00bba53c105a5",
+    "url": "/MyPortfolio/static/media/react-portfolio-thumbail.a0d1b76b.jpg"
+  }
+]);
